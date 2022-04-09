9d078f949e702db979ce198ba8379b28e1b8b4e3
Harshal0902 <harshalraikwar07@gmail.com>
2022-04-10 01:27:02 +0530

    ✨ Add localization, text-to-speech and Razorpay

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,40 +2,44 @@ import React from 'react'
 import TravelImg from '../assets/travel.svg'
 import FeatImg from '../assets/features.svg'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 export default function Home() {
+
+  const { t } = useTranslation();
+
   return (
     <div className='md:mx-28 mx-4 text-white pt-10 pb-12'>
 
-      <div className='md:grid md:grid-cols-2 items-center pt-10'>
+      <div className='md:grid md:grid-cols-2 items-center pt-10 pb-4'>
         <div className=''>
-          <h1 className='text-3xl md:text-6xl'>Who we are</h1>
-          <p className='text-xl md:text-2xl py-4 tracking-wider'></p>
+          <h1 className='text-3xl md:text-6xl'>{t('homeTitle')}</h1>
+          <p className='text-xl py-4 tracking-wider text-justify'>{t('hoemAbout')}</p>
 
           <Link to="/upload">
-            <button className='bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl'>Upload Menue</button>
+            <button className='bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl'>{t('homeUpdate')}</button>
           </Link>
 
         </div>
         <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
-          <img src={TravelImg} alt="img" width="350" height="350" />
+          <img src={TravelImg} alt="img" width="450" height="450" />
         </div>
       </div>
 
       <div className='md:grid md:grid-cols-2 pt-12 items-center'>
         <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
-          <img src={FeatImg} alt="img" width="350" height="350" />
+          <img src={FeatImg} alt="img" width="400" height="400" />
         </div>
         <div className=''>
-          <h1 className='text-3xl md:text-6xl'>What else do we have</h1>
-          <p className='text-xl md:text-2xl py-4 tracking-wider'> Don't Dis My Ability is a web app where you can:
+          <h1 className='text-3xl md:text-6xl'>{t('homwWhatElse')}</h1>
+          <p className='text-xl md:text-2xl py-4 tracking-wider'> {t('homeDesc')}
           </p>
           <ul className="text-xl">
-            <li className="list-disc">Allow micropayments to be made using Coil</li>
-            <li className="list-disc">Classify the food as vegetarian or non-vegetarian</li>
-            <li className="list-disc">Upload the menu and extract the information from it</li>
-            <li className="list-disc">Convert the extracted text from the menu into speech</li>
-            <li className="list-disc">Get nutrition information about the food, such as its ingredients and its calories</li>
+          <li className="list-disc">{t('homePt1')}</li>
+            <li className="list-disc">{t('homePt2')}</li>
+            <li className="list-disc">{t('homePt3')}</li>
+            <li className="list-disc">{t('homePt4')}</li>
+            <li className="list-disc">{t('homePt5')}</li>
           </ul>
         </div>
       </div>
--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -1,5 +1,67 @@
+import React from 'react';
+// import Speech from 'react-speech';
+import { FcSpeaker } from "react-icons/fc";
+import { useSpeechSynthesis } from "react-speech-kit";
+
 const Results = (props) => {
+
   const { code } = props;
+
+  const [value, setValue] = React.useState("");
+  const { speak } = useSpeechSynthesis();
+
+  // var formatter = new Intl.NumberFormat("en-US", {
+  //   style: "currency",
+  //   currency: "INR",
+  //   minimumFractionDigits: 0,
+  // });
+
+  const loadScript = (src) => {
+    return new Promise((resovle) => {
+      const script = document.createElement("script");
+      script.src = src;
+
+      script.onload = () => {
+        resovle(true);
+      };
+
+      script.onerror = () => {
+        resovle(false);
+      };
+
+      document.body.appendChild(script);
+    });
+  };
+
+  const displayRazorpay = async (amount) => {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("You are offline... Failed to load Razorpay SDK");
+      return;
+    }
+
+    const options = {
+      key: "Enter_you_API_here",
+      currency: "INR",
+      amount: amount * 100,
+      name: "Don't Dis My Ability",
+      description: "Thanks for purchasing",
+
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+        alert("Payment Successfully");
+      },
+      prefill: {
+        name: "Don't Dis My Ability",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  };
 
   return (
     <>
@@ -10,9 +72,35 @@ const Results = (props) => {
             <div className="bg-yellow-500 h-4 w-4 rounded-full"></div>
             <div className="bg-green-500 h-4 w-4 rounded-full"></div>
           </div>
-          {code}
+
+          <textarea
+            rows="10"
+            value={code}
+            onChange={(e) => setValue(e.target.value)}
+            className="w-full border-none active:border-none resize-none"
+          ></textarea>
+
+          {/* {code} */}
+
+          <div className='flex flex-row '>
+            <button onClick={() => speak({ text: value })}><FcSpeaker className="h-8" /></button>
+            {/* <button>
+              <FcSpeaker className="h-8" />
+              <Speech text={code} stop={true}
+                pause={true}
+                resume={true}
+                className="cursor-pointer z-10 w-8 h-1" />
+            </button> */}
+          </div>
+
         </div>
+
+        <div className='py-8 ml-12'>
+          <button className='bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl text-white' onClick={() => displayRazorpay(5)}>Pay amount</button>
+        </div>
+
       </div>
+
     </>
   );
 };
